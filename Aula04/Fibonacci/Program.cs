1e32ccd04a40f2d0b584cb17ddb60ba6e142c6a5
Hugo Carvalho <huguinho66@live.com>
2022-03-13 18:38:07 +0000

    ex aula4 added

---
+++ Aula04/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p;
+            p = new Program();
+            Console.WriteLine("Insert the number for fibonacci");
+            int number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(p.Fibonacci(number));
+            
+        }
+
+        private int Fibonacci(int _n)
+        {
+            if (_n <= 2)
+                return 1;
+            else
+                return Fibonacci(_n - 1) + Fibonacci(_n - 2);
+            
+        }
+    }
+}
---
+++ Aula04/MyFirstMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyFirstMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p;
+            p = new Program();
+            p.CountTo10();
+            p.CountTo10();
+
+            Console.WriteLine("Insert the number to Count");
+
+            int number = int.Parse(Console.ReadLine());
+            p.CountToN(number);
+            Console.WriteLine("Insert the number to Count");
+            int number2 = int.Parse(Console.ReadLine());
+            p.CountToN(number2);
+            p.CountToN(10,20);
+        }
+
+        private void CountTo10()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private void CountToN(int _n)
+        {
+            for (int i = 0; i <= _n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_n1"></param>
+        /// <param name="_n2"></param>
+        private void CountToN(int _n1, int _n2)
+        {
+            for (_n1 = _n1; _n1 < _n2; _n1++)
+            {
+                Console.WriteLine(_n1);
+            }
+        }
+    }
+}
